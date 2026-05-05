f2b26ed2c8832d43d7b9dfe48f6291bb9229ac92
Brenda <bbrendabbrandao@gmail.com>
2026-05-05 20:34:57 -0300

    2026-05-05

---
+++ dia_03/tabuada.py
@@ -0,0 +1,12 @@
+#Calculando a tabuada do 2 
+
+#while 
+numero = 2 
+count = 1 
+while count <= 100: 
+    print(numero, "x", count, "=", numero * count)
+    count += 1 # count = count + 1  
+
+print("Acabou!")
+
+
