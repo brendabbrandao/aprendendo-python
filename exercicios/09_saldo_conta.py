043e60ab764c9cfd0aaaff6225cf10ba62e0240a
Brenda <bbrendabbrandao@gmail.com>
2026-05-08 20:28:50 -0300

    exercicio saldo_conta

---
+++ exercicios/09_saldo_conta.py
@@ -0,0 +1,3 @@
+#Faça um programa que receba uma quantidade indefinida de valores correspondentes a "saldo em conta",
+#mas, quando o usuário apertar "enter", sem digitar valor algum, o programa para de receber valores,
+#e exibe a soma de todos os valores digitados anteriormente 
