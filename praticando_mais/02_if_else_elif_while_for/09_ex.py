6f4ab7584db6ac2fdf74f64b1d1f4aac2b146608
Brenda <bbrendabbrandao@gmail.com>
2026-05-11 22:19:54 -0300

    ex 9

--- praticando_mais/02_if_else_elif_while_for/09_ex.py
+++ praticando_mais/02_if_else_elif_while_for/09_ex.py
@@ -1,5 +1,5 @@
 #Comece com o número 1. 
-#Fique dobrando ele (multiplicando por 2) enquanto for menor que 1000. Mostre cada valor e, ao final, quantas vezes dobrou.
+#Fique dobrando ele (multiplicando por 2) enquanto for menor que 1000. Mostre cada valor e, ao final, quantas vezes dobrou..
 
 inicio = 1 
 vezes = 0 
