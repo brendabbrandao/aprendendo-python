bf0daa09e4ec600fca1308f430e8548070c99c94
Brenda <bbrendabbrandao@gmail.com>
2026-05-11 22:17:43 -0300

    a pratica leva a perfeição

---
+++ praticando_mais/02_if_else_elif_while_for/09_ex.py
@@ -0,0 +1,11 @@
+#Comece com o número 1. 
+#Fique dobrando ele (multiplicando por 2) enquanto for menor que 1000. Mostre cada valor e, ao final, quantas vezes dobrou.
+
+inicio = 1 
+vezes = 0 
+
+while inicio < 1000:
+    print(inicio)
+    inicio = inicio * 2 
+    vezes = vezes + 1 
+print("Dobrou esse tanto de vezes: ", vezes)
