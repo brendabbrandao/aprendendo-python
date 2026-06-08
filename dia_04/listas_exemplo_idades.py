c194e524c11d40b772f67b2aa15db78fc30fb165
Brenda <bbrendabbrandao@gmail.com>
2026-06-08 20:55:58 -0300

    exemplos de listas

---
+++ dia_04/listas_exemplo_idades.py
@@ -0,0 +1,33 @@
+#vou pegar várias idades dos usuários 
+
+#métodos - ações que os objetos no python pode ter 
+
+#um dos métodos das listas -> adicionar elementos novos 
+
+# idades = [17, 32, 56, 87]
+# print(idades)
+
+#para adicionar um elemento novo nessa lista
+# idades.append(42)
+# print(idades)
+
+#a própria lista foi modificada adicionando um último elemento no final dela! 
+
+idades = []
+
+while True:
+    idade = input("Entre com a idade: ")
+    if idade == "":
+        break
+    idades.append(int(idade))
+
+media = sum(idades) / len(idades)
+minimo = min(idades)
+maximo = max(idades)
+qtde = len(idades)
+
+print("Média: ", media)
+print("Mínimo: ", minimo)
+print("Máximo: ", maximo)
+print("Qtde: ", qtde)
+
