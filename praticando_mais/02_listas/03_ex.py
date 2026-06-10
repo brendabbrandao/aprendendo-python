151c4fedc0b89e7024a33ddff3f5c1307e751946
Brenda <bbrendabbrandao@gmail.com>
2026-06-09 21:11:41 -0300

    ex 3  de listas pra praticar mais

---
+++ praticando_mais/02_listas/03_ex.py
@@ -0,0 +1,7 @@
+#Crie uma lista com 5 números. Use um for para percorrer a lista e exibir cada número multiplicado por 2.
+
+numeros = [1, 2, 3, 4, 5]
+
+for i in numeros:
+    multiplicacao = i * 2 
+    print("O número:", i, "multiplicado por 2 é: ", multiplicacao)
