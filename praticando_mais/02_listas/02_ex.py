2928e7cc60271f5bb7a4cc980b592093045e8ae6
Brenda <bbrendabbrandao@gmail.com>
2026-06-09 21:04:54 -0300

    ex 2 de listas pra praticar mais

---
+++ praticando_mais/02_listas/02_ex.py
@@ -0,0 +1,9 @@
+# Crie uma lista com 3 nomes. Adicione um novo nome ao final, depois remova o primeiro nome. 
+# Exiba a lista após cada alteração.
+
+nomes = ['brenda', 'ernesto', 'otto']
+nomes.append('olivia')
+nomes.append('caneta')
+print(nomes)
+nomes.remove('caneta')
+print(nomes)
