29abe99f9340153affc162aef52b00697062718e
Brenda <bbrendabbrandao@gmail.com>
2026-06-09 21:00:22 -0300

    ex 1 de listas pra praticar mais

---
+++ praticando_mais/02_listas/01_ex.py
@@ -0,0 +1,9 @@
+#Crie uma lista com 4 frutas. Exiba a lista inteira.
+#Depois, exiba a primeira e a última fruta 
+
+frutas = ['maça', 'laranja', 'morango']
+print(frutas)
+
+print(frutas[0])
+
+print(frutas[-1])
