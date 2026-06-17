fa01955b6ce838814fe20a94766a5b52413289a1
Brenda <bbrendabbrandao@gmail.com>
2026-06-17 19:38:09 -0300

    dic - ex 01 - praticando mais

---
+++ praticando_mais/03_dicionarios/01_ex.py
@@ -0,0 +1,11 @@
+#Crie um dicionário com as informações de uma pessoa: nome, idade e cidade. 
+# # Exiba cada informação separadamente com uma frase, como: "Nome: Ana".
+
+info = {
+    "Nome":"Ana",
+    "Idade":54,
+    "Cidade":"Guaratinguetá"
+}
+print("Nome: ", info["Nome"])
+print("Idade: ", info["Idade"])
+print("Cidade: ", info["Cidade"])
