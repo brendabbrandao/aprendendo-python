0153a668c63d7cd48352fdd258d8acdcb2ff5ff8
Brenda <bbrendabbrandao@gmail.com>
2026-06-18 20:52:30 -0300

    aula funcoes

---
+++ dia_06/01_funcoes.py
@@ -0,0 +1,7 @@
+#Funções matemáticas -> f(x) = 1 + x 
+
+#Como implementar uma função no python -> def é a palavra reservada da função
+def f(x):
+    resultado = 1 + x
+    return resultado #função podem ou não retornar coisas | quero retornar um valor/variável
+f(0)
