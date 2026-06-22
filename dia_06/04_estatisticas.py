462457fd7a9291811ebe26e7d33e9c1b560811c2
Brenda <bbrendabbrandao@gmail.com>
2026-06-22 20:53:36 -0300

    aula funcoes

--- dia_06/04_estatisticas.py
+++ dia_06/04_estatisticas.py
@@ -10,3 +10,12 @@ a = float(input("Entre com o valor de A: "))
 b= float(input("Entre com o valor de B: "))
 
 print("A média é: ", media(a,b))
+
+#Para colocar mais um argumento (opcional)
+def media(a:float, b:float, c=0)->float:   #o argumento opcional tem que vir depois dos argumentos obrigatórios
+    return soma(a,b,c) / 3 
+
+#args
+def soma(a:float, b:float, *args)->float:   #tudo que passar na função sem ser valor obrigatório o args vai absorver
+    valores = [a,b] + args 
+    return sum(valores)
