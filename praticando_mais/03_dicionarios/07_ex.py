fd06a6294890dbd1f33e4beb703047b2cad5681f
Brenda <bbrendabbrandao@gmail.com>
2026-06-23 21:10:02 -0300

    exercicio de dict

---
+++ praticando_mais/03_dicionarios/07_ex.py
@@ -0,0 +1,10 @@
+# Você tem um dicionário com os dados de um aluno:
+#Imprima cada informação no formato:
+# nome: Ana
+# idade: 20
+# curso: Python
+
+aluno = {"nome": "Ana", "idade": 20, "curso": "Python"}
+
+for info in aluno:
+    print(info, aluno[info])
