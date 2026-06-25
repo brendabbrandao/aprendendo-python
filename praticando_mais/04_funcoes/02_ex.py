749e5006762a691079e854a50275d050a2bc493b
Brenda <bbrendabbrandao@gmail.com>
2026-06-25 20:44:32 -0300

    praticando mais dict

---
+++ praticando_mais/04_funcoes/02_ex.py
@@ -0,0 +1,7 @@
+# Crie uma função chamada saudacao_nome que recebe um parâmetro nome e imprime "Olá, Ana!" (usando o nome recebido).
+
+
+def saudacao_nome(nome):
+    print("Olá", nome)
+
+saudacao_nome("Brenda")
