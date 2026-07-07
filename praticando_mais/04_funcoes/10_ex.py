0e78b7f7f0cc5358974e50e29e928e26c2217c39
Brenda <bbrendabbrandao@gmail.com>
2026-07-06 21:38:04 -0300

    praticando mais funções

---
+++ praticando_mais/04_funcoes/10_ex.py
@@ -0,0 +1,9 @@
+# Crie uma função calcular_potencia(base, expoente=2) que retorna base elevado a expoente. 
+# Se expoente não for informado, deve usar 2. Teste chamando a função de duas formas.
+
+def calcular_potencia(base, expoente=2):
+    return base ** expoente
+
+resultado = calcular_potencia(3)
+
+print(resultado)
